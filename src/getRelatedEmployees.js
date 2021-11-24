c6dbbd09f67613b37db18fa0d6c9e27fe83e9b16
george lucas <glalmeida.dev@gmail.com>
2021-11-24 19:46:10 -0300

    Arrumando requisitos

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const employe = data.employees.find((empregado) => empregado.id === id);
+  if (employe.managers.length !== 0) {
+    return false;
+  }
+  return true;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
 }
+
+console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
