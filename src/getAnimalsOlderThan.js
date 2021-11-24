2eeb586e7c2683c29217bbd2e58002e18438a7ce
george lucas <glalmeida.dev@gmail.com>
2021-11-24 17:12:39 -0300

    requisito 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,10 +2,9 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   const especie = data.species.find((animais) => animais.name === animal);
+  console.log(especie);
   const olderthan = especie.residents.every((animals) => animals.age >= age);
   return olderthan;
 }
-
-console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
