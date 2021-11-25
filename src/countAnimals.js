e54e9dbc97059ee747793e13aac123af5c6eb3a9
george lucas <glalmeida.dev@gmail.com>
2021-11-25 17:20:32 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(animals) {
+  const speciee = {};
+  if (animals === undefined) {
+    data.species.forEach((animal) => {
+      speciee[animal.name] = animal.residents.length;
+    });
+    return speciee;
+  }
+  const arrayAnimals = Object.values(animals);
+  const h = data.species.find((especie) => especie.name === animals.specie).residents;
+  if (arrayAnimals.length === 1) {
+    return h.length;
+  }
+  const l = h.filter((elemento) => elemento.sex === arrayAnimals[1]);
+  return l.length;
 }
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
