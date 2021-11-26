2e878f047331b819eacf1af6274b8c26fbdd6dc3
george lucas <glalmeida.dev@gmail.com>
2021-11-25 23:17:09 -0300

    Requisito 6

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,6 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
+  console.log(ids);
   if (ids !== undefined) {
     const specie = data.species.filter((elemento) => ids.includes(elemento.id));
     return specie;
